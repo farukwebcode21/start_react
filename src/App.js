b42b3de3a3f65542f3d8ebcf430a46de122dc011
farukwebcode21 <farukahmadmk@yahoo.com>
2023-02-17 08:21:44 +0600

    react router dom new version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/", element: <Main/>,
+      children:[
+        {path: '/', element:<Home/>},
+        {path: '/home', element:<Home/>},
+        {path:'/products', element:<Products/>}
+      ]
+  },
+    {path:'about', element:<About/>},
+    {path:'*', element:(<div>This route not found</div>)}
+
+  ]);
   return (
     <div className="App">
-      <h1>Header Testing React Js</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const About = () => {
+  return (
+    <div>
+        <Header/>
+        <h1>This is about pages</h1>
+    </div>
+  )
+}
+
+export default About
